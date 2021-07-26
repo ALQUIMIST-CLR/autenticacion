640d1d935fc216f0b24045326813c7a556ed9f31
ALQUIMIST-CLR <carolynelespay@gmail.com>
2021-07-25 23:51:31 -0400

    autenticacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def authenticate_admin!
+    unless current_user.present? && current_user.admin?
+      redirect_to root_path, alert: "You must be an admin to access this page"
+    end
+  end
 end
